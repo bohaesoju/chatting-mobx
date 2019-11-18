ff4462a19732fd765dffb77d02e651deb780b303
kimuyjoong <bohaesoju@gmail.com>
2019-11-18 18:05:59 +0900

    apply immer draft

--- src/reducers/ChattingList.js
+++ src/reducers/ChattingList.js
@@ -2,33 +2,31 @@ export const CHATTINGLIST_REQUEST = 'CHATTINGLIST_REQUEST';
 export const CHATTINGLIST_SUCCESS = 'CHATTINGLIST_SUCCESS';
 export const CHATTINGLIST_FAILURE = 'CHATTINGLIST_FAILURE';
 
+const { produce } = require('immer');
+
 const initialState = {
     data: [],
     isFetchChattingList: false
 };
 
 const chattingList = (state = initialState, action) => {
-    switch(action.type){
-        case CHATTINGLIST_REQUEST:
-            return {
-                ...state,
-                isFetchChattingList: false
-            };
-        case CHATTINGLIST_SUCCESS:
-            return {
-                ...state,
-                data: action.data,
-                isFetchChattingList: true
-            };
-        case CHATTINGLIST_FAILURE:
-            return {
-                ...state,
-                data: null,
-                isFetchChattingList: false
-            };
-        default:
-            return Object.assign({}, state);
-    }
+    return produce (state, (draft) => {
+        switch(action.type){
+            case CHATTINGLIST_REQUEST:
+                draft.isFetchChattingList = false;
+                break;
+            case CHATTINGLIST_SUCCESS:
+                draft.data = action.data;
+                draft.isFetchChattingList = true;
+                break;
+            case CHATTINGLIST_FAILURE:
+                draft.data = null;
+                draft.isFetchChattingList = false;
+                break;
+            default:
+                return Object.assign({}, state);
+        }
+    })
 };
 
 export default chattingList;
